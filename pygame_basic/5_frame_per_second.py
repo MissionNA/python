6669de8dfd6c62bce661eef6f2f2eaed7446a0cf
MissionNA <121701096+MissionNA@users.noreply.github.com>
2023-02-18 12:02:33 +0900

    2023-02-18

--- pygame_basic/5_frame_per_second.py
+++ pygame_basic/5_frame_per_second.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode((screen_width,screen_heigth)) #튜플로 고정
 #화면 타이틀 설정
 pygame.display.set_caption('한문철 게임') #게임이름
 
+#FPS
+clock = pygame.time.Clock()
+
 #배경이미지 불러오기
 background=pygame.image.load('D:\python\pygame_basic\\background.png')
 
@@ -28,33 +31,36 @@ character_y_pos= screen_heigth-character_heigth
 # 이동할 좌표
 to_x = 0
 to_y = 0
-
-
+#이속
+character_speed = 2
+to_WHY = 0.054321
 
 #이벤트 루프
 running = True #게임이 진행중인가?
 while running: #반복문
+    dt = clock.tick(60) #게임화면의 초당 프레임 수를 설정
+
     for event in pygame.event.get(): #어떤 이벤트(동작)가 발생하였는가?
         if event.type == pygame.QUIT: #창이 닫히는 이벤트가 발생하였는가?
             running=False #게임이 진행중이 아님
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                to_x -= 5
+                to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
-                to_x += 5
+                to_x += character_speed
             elif event.key == pygame.K_UP:
-                to_y -= 5
+                to_y -= character_speed
             elif event.key == pygame.K_DOWN:
-                to_y += 5
+                to_y += to_WHY
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     to_x =0
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     to_y =0
-    character_x_pos += to_x
-    character_y_pos += to_y
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
 
 
 
